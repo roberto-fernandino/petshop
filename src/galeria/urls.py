240161e94d22b3887368fd01442710449686e153
DiegoCheib <diegocheib@gmail.com>
2023-07-31 19:21:44 -0300

    Atualizando Navbar/Incompleta

--- src/galeria/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from galeria import views
-
-app_name = 'galeria'
-
-urlpatterns = [
-    path('', views.galeriaview, name='galeria')
-]
--- src/loja/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lojaview(request, *args, **kwargs):
-    return render(request, "loja.html", {})
-
-def produtoview(request, *args, **kwargs):
-    return render(request, 'produto.html', {})
