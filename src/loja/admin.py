dab142b40153d5e18f74f4860ec1e9ff9104ee4b
roberto-fernandino <romfernandino@gmail.com>
2023-08-05 15:05:52 -0300

    Made product model not yet done

--- src/loja/admin.py
+++ src/loja/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Produto
 # Register your models here.
+
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ["nome_produto", "setor", "avaliacao"]
+    ordering = ["preco"]
+    list_filter = ["preco", "setor"]
+    search_fields = ["nome_produto"]
+    filter_horizontal = []
+
+admin.site.register(Produto, ProdutoAdmin)
--- src/loja/models.py
+++ src/loja/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=100)
+    setor = models.CharField(max_length=50)
+    preco = models.FloatField()
+    descricao = models.TextField(null=True, blank=False)
+    imagem = models.ImageField(blank=False, null=True)
+
+    def __str__(self):
+        return f" {self.nome_produto} | {self.setor} "
+    
+
+
+
