f60972b68192633285ef8227f54c9ce26a76c59b
roberto-fernandino <romfernandino@gmail.com>
2023-08-09 18:05:54 -0300

    Added get filters to Products page, filtering by category, price, and name

--- src/loja/models.py
+++ src/loja/models.py
@@ -4,9 +4,18 @@ from loja.funcs import product_image_path
 
 # Create your models here.
 
+class Category(models.Model):
+    nome = models.CharField(max_length=60, default=None, blank=False, null=False)
+    def __str__(self) -> str:
+        return f"{self.nome}"
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Produto(models.Model):
     nome = models.CharField(max_length=50, default="Produto", unique=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     descricao = models.TextField()
     img = models.ImageField(upload_to=product_image_path)
     estoque = models.IntegerField(default=0, null=False, blank=False)
--- src/loja/views.py
+++ src/loja/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from loja.models import Produto
+from loja.models import Produto, Category
 
 # Create your views here.
 def lojaview(request, *args, **kwargs):
     produtos = Produto.objects.all()
-    return render(request, "loja.html", {"produtos": produtos})
+    categorias = Category.objects.all()
+
+    #category filter
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        produtos = produtos.filter(category=categoria_id)
+    
+    #nome filter
+    nome = request.GET.get('nome')
+    if nome:
+        produtos = produtos.filter(nome__icontains=nome)
+    
+    #filtra por preco maximo
+    preco_max = request.GET.get("preco_max")
+    if preco_max:
+        produtos = produtos.filter(preco__lte=float(preco_max))
+    
+    
+
+
+    return render(request, "loja.html", {"produtos": produtos, "categorias": categorias})
 
 def produtoview(request, *args, **kwargs):
     return render(request, 'produto.html', {})
